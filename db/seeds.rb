ee2bc992f78775fcabdaf8a0c5a37e37935d0f0d
Pavel Liser <liser.pavel@gmail.com>
2018-04-04 11:44:41 +0800

    changed some configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.delete_all
+
+10.times do Restaurant.create(name: Faker::HitchhikersGuideToTheGalaxy.planet,
+                           address: Faker::HitchhikersGuideToTheGalaxy.location,
+                           phone_number: Faker::PhoneNumber.phone_number,
+                           category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+puts "restaurant added"
+end
